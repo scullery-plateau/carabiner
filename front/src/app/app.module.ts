416a8669709f937459a4b7780b0451f2813c5e3f
Daniel Johnson <daniel.johnson@surescripts.com>
2019-06-04 19:50:56 -0500

    restructuring initial structure of cobblestone per new layout designs

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,11 +6,7 @@ import { AppComponent } from './app.component';
 import { CobblestoneComponent } from './cobblestone/cobblestone.component';
 import { MastermoldComponent } from './mastermold/mastermold.component';
 import { SpritelyComponent } from './spritely/spritely.component';
-import { MapsComponent } from './maps/maps.component';
-import { PalettesComponent } from './palettes/palettes.component';
-import { TilesComponent } from './tiles/tiles.component';
-import { CharactersComponent } from './characters/characters.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { PixelCanvasComponent } from './pixel-canvas/pixel-canvas.component';
 import { PixelPainterComponent } from './pixel-painter/pixel-painter.component';
 
@@ -20,10 +16,6 @@ import { PixelPainterComponent } from './pixel-painter/pixel-painter.component';
     CobblestoneComponent,
     MastermoldComponent,
     SpritelyComponent,
-    MapsComponent,
-    PalettesComponent,
-    TilesComponent,
-    CharactersComponent,
     PixelCanvasComponent,
     PixelPainterComponent
   ],
