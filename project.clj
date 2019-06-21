8bafa2eea58c117b15978f13b0953acc649c685d
Daniel Johnson <daniel.johnson@surescripts.com>
2019-06-21 15:59:51 -0500

    adding hiccup and htmltopdf

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
                  [http-kit "2.2.0"]
                  [environ "1.0.0"]
                  [metosin/compojure-api "1.1.11"]
-                 [ring/ring-mock "0.3.2"]]
+                 [ring/ring-mock "0.3.2"]
+                 [hiccup "1.0.5"]
+                 [clj-htmltopdf "0.1-alpha6"]]
   :min-lein-version "2.0.0"
   :main carabiner.server
   :plugins [[environ/environ.lein "0.3.1"]]
