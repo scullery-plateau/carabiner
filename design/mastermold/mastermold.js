2232d1d007bd4f7f744448c7a01685367c919685
Voltron42 <dajohnson1978@gmail.com>
2020-01-15 07:10:34 -0600

    working state. need css for printing

--- design/mastermold/mastermold.js
+++ design/mastermold/mastermold.js
@@ -5,11 +5,35 @@
         })
     };
     let listItem = function(img) {
-        return `<li><span>${img.filename}</span><input type="number" min="1" value="1" onchange="mm.updateCount(this,'${img.filename}')"/><button class="nes-btn is-error" onclick="mm.removeImage('${img.filename}')">X</button></li>`;
+        return `<tr><td>${img.filename}</td><td><input type="number" min="1" value="1" onchange="mm.updateCount(this,'${img.filename}')"/></td><td><button class="nes-btn is-error" onclick="mm.removeImage('${img.filename}')">X</button></td></tr>`;
     };
-    let drawFrame = function() {};
-    let drawImg = function(url,index) {
-
+    let frameRect = {
+      small:[25,[0,325]],
+      large:[150,[25,175]],
+      steps:[350,100]
+    }
+    let drawRect = function(x,y,w,h) {
+      return `<rect x="${x}" y="${y}" width="${w}" height="${h}" fill="white" stroke="black" stroke-width="2"></rect>`;
+    };
+    let drawFrame = function() {
+      return "?".repeat(20).split("").map((c,i) => {
+        let x = (i % 2) * frameRect.steps[0];
+        let y = Math.floor(i / 2) * frameRect.steps[1];
+        let out = [];
+        ["small","large"].forEach((spec) => {
+          let width = frameRect[spec][0];
+          frameRect[spec][1].forEach((xOff) => {
+            out.push(drawRect(x + xOff,y,width,100));
+          });
+        });
+        return out;
+      });
+    };
+    let drawImg = function(url,i) {
+      let x = (i % 2) * frameRect.steps[0];
+      let y = Math.floor(i / 2) * frameRect.steps[1];
+      return `<image href="${url}" x="${x + 55}" y="${y - 20}" width="90" height="140" transform="rotate(90 ${x + 100} ${y + 50})"></image>
+      <image href="${url}" x="${x + 205}" y="${y - 20}" width="90" height="140"  transform="rotate(-90 ${x + 250} ${y + 50})"></image>`;
     };
     let drawPage = function(page) {
         return `<div class="page"><svg width="7.5in" height="10in" viewBox="0 0 700 1000">
@@ -65,13 +89,13 @@ ${page.urls.map(drawImg).join("\n")}
                     urls:urls.splice(0,20)
                 })
             }
-            ui[canvasId].innerHTML = pages.map(drawPage);
+            ui[canvasId].innerHTML = pages.map(drawPage).join("/n");
         };
         this.init = function() {
             populateUI(ui,[fileId,listId,canvasId]);
         };
         this.addImage = function() {
-            let files = ui[fileId].files
+            let files = Array.from(ui[fileId].files);
             console.log(files);
             if (files.length > 0) {
                 loadImages(files,() => {
