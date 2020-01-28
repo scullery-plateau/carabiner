a8fadd3ef514de44c7927b0a0b7475948aae4542
Voltron42 <dajohnson1978@gmail.com>
2020-01-28 06:10:04 -0600

    rearranging outfitter data to use markers

--- test/carabiner/outfitter/shapes_test.clj
+++ test/carabiner/outfitter/shapes_test.clj
@@ -9,25 +9,27 @@
 (defn wrap-svg [[id svg]]
   [:div {:id id :alt id :title id} svg])
 
-(defn read-svg [out ^File file]
-  (let [id (read-string (first (str/split (.getName file) #"\.")))
-        svg (-> (x/from-xml (slurp file))
-                (update 1 select-keys [:width :height])
-                (update 1 assoc :id id))]
-    (assoc out id svg)))
+(defn remove-id-from-svg [svg]
+  (update svg 1 dissoc :id))
 
-(defn build-tree-recurse [^File file ^String dir-name ^String table-name]
+(defn read-svg
+  ([out ^File file]
+   (let [[id svg] (read-svg file)
+         svg (update svg 1 assoc :id id)]
+     (assoc out id svg)))
+  ([^File file]
+   (let [id (read-string (first (str/split (.getName file) #"\.")))
+         svg (-> (x/from-xml (slurp file))
+                 (update 1 select-keys [:width :height]))]
+     [id svg])))
+
+(defn build-tree-recurse [^File file  ^String dir-name ^String table-name markers]
   (let [convo #_(with-out-str (pp/pprint %)) h/to-html
         children (.listFiles file)
         names (set (map #(.getName ^File %) children))]
     (pp/pprint names)
     (if (contains? names dir-name)
-      (let [svgs (reduce read-svg (sorted-map) (.listFiles (File. file dir-name)))
-            mapping (reduce-kv
-                      #(assoc %1 %2 (mapv :id %3))
-                      {}
-                      (group-by #(select-keys % [:width :height])
-                                (map second (vals svgs))))]
+      (let [svgs (reduce read-svg (sorted-map) (.listFiles (File. file dir-name)))]
         (println "has shapes: " + (.getAbsolutePath file))
         (if (contains? names table-name)
           (let [table (read-string (slurp (File. file table-name)))]
@@ -46,17 +48,20 @@
                                      [:td (wrap-svg [cell (get svgs cell)])])
                                    %))
                               table))]])))
-          (do
-            (spit (File. file "mapping.edn") mapping)
-            (spit (File. file "compiled.html")
-                  (convo
-                    [:html
-                     [:head [:title "Gallery"]]
-                     (into [:body] (map wrap-svg svgs))]
-                    )))))
+          (spit (File. file "compiled.html")
+                (convo
+                  [:html
+                   [:head
+                    [:title "Gallery"]
+                    [:style "body { background-color: green; }"]]
+                   (into [:body] (map wrap-svg svgs))]))))
       (doseq [child children]
-        (build-tree-recurse child dir-name table-name)))))
+        (build-tree-recurse child dir-name table-name markers)))))
 
 (deftest compile-shapes
-  (let [root (File. "design/outfitter/items")]
-    (build-tree-recurse root "shapes" "table.edn")))
+  (let [root (File. "design/outfitter/items/body")
+        markers (->> (File. "design/outfitter/items/markers/body")
+                     (.listFiles)
+                     (map read-svg)
+                     (into {}))]
+    (build-tree-recurse root "shapes" "table.edn" markers)))
