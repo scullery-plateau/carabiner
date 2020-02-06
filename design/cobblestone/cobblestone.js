154ac84ba3064e4d394e26b916d5365b1d11182b
Voltron42 <dajohnson1978@gmail.com>
2020-02-05 21:07:16 -0600

    building out cobblestone redesign and updating footer

--- design/cobblestone/cobblestone.js
+++ design/cobblestone/cobblestone.js
@@ -1,10 +1,11 @@
 (function(){
-    window.Cobblestone = function(loaderId,selectorId,inputId,canvasId) {
+    window.Cobblestone = function(loaderId,selectorId,inputId,errorsId,reloadBtnId,canvasId) {
         let ui = {};
         let data = {};
         let selectedId = undefined;
         let render = function() {
-            ui[inputId].value = data[selectorId];
+            ui[inputId].value = data[selectedId];
+            console.log(data);
         };
         this.init = function() {
             [loaderId,selectorId,inputId,canvasId].forEach((id) => {
@@ -14,19 +15,34 @@
         this.loadFile = function() {
             let file = ui[loaderId].files[0];
             let reader = new FileReader();
-            reader.addEventListener("load",() => {
-                let blocks = reader.result.split('\r').join('').split('\n\n');
+            reader.onload = function() {
+                console.log("loading file");
+                let textdata = reader.result;
+                console.log(textdata);
+                let blocks = textdata.split('\r').join('').split('\n\n');
                 ['fullMap','paging','transforms','palettes'].forEach((key) => {
                     data[key] = blocks.shift();
+                    let option = document.createElement('OPTION');
+                    option.text = key;
+                    option.value = key;
+                    ui[selectorId].appendChild(option);
                 });
+                let group = document.createElement('OPTGROUP');
+                group.label = "tiles";
                 blocks.forEach((block) => {
                     let rows = block.split("\n");
                     let tileId = rows.shift();
-                    data["tiles."+tileId] = rows.join('\n');
+                    let tileKey = "tiles."+tileId
+                    data[tileKey] = rows.join('\n');
+                    let option = document.createElement('OPTION');
+                    option.text = tileId;
+                    option.value = tileKey;
+                    group.appendChild(option);
                 });
+                ui[selectorId].appendChild(group);
                 selectedId = "fullMap";
                 render();
-            },false);
+            };
             reader.readAsText(file);
         };
         this.saveFile = function() {
